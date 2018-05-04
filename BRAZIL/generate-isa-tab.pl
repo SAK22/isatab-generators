fd3f1eb25a3428e2ebfda1bd4af35909b1af3094
Sarah Kelly <s.kelly@imperial.ac.uk>
2018-05-04 17:48:58 +0100

    completed script for Brazil longitudinal data, needs a couple of corrections

--- BRAZIL/generate-isa-tab.pl
+++ BRAZIL/generate-isa-tab.pl
@@ -62,11 +62,13 @@ my @a_species = ( [ 'Sample Name', 'Assay Name', 'Description', 'Protocol REF', 
 
 my @a_collection = ( [ 'Sample Name', 'Assay Name', 'Protocol REF', 'Date', 'Characteristics [Collection site (VBcv:0000831)]', 'Term Source Ref', 'Term Accession Number', 'Characteristics [Collection site latitude (VBcv:0000817)]', 'Characteristics [Collection site longitude (VBcv:0000816)]', 'Comment [collection site coordinates]', 'Characteristics [Collection site locality (VBcv:0000697)]', 'Characteristics [Collection site country (VBcv:0000701)]' ] ); # 'Characteristics [Collection site location (VBcv:0000698)]', 'Characteristics [Collection site village (VBcv:0000829)]', 'Characteristics [Collection site locality (VBcv:0000697)]', 'Characteristics [Collection site suburb (VBcv:0000845)]', 'Characteristics [Collection site city (VBcv:0000844)]', 'Characteristics [Collection site county (VBcv:0000828)]', 'Characteristics [Collection site district (VBcv:0000699)]', 'Characteristics [Collection site province (VBcv:0000700)]', 'Characteristics [Collection site country (VBcv:0000701)]' ] );
 
-my @a_IR_WHO = ( ['Sample Name', 'Assay Name','Protocol REF', 'Parameter Value [group1.insecticidal substance]', 'Term Source Ref', 'Term Accession Number', 'Parameter Value [group1.concentration]', 'Unit', 'Term Source Ref', 'Term Accession Number', 'Parameter Value [duration of exposure]', 'Unit', 'Term Source Ref', 'Term Accession Number', 'Characteristics [sample size (VBcv:0000983)]', 'Raw Data File' ] );
+my @a_IR_WHO = ( ['Sample Name', 'Assay Name','Protocol REF', 'Parameter Value [group1.insecticidal substance]', 'Term Source Ref', 'Term Accession Number', 'Parameter Value [group1.concentration]', 'Unit', 'Term Source Ref', 'Term Accession Number', 'Parameter Value [duration of exposure]', 'Unit', 'Term Source Ref', 'Term Accession Number', 'Characteristics [sample size (VBcv:0000983)]','Comment [note]', 'Raw Data File' ] );
 
 my @p_IR_WHO = ( ['Assay Name', 'Phenotype Name', 'Observable', 'Term Source Ref', 'Term Accession Number', 'Attribute', 'Term Source Ref', 'Term Accession Number', 'Comment [note]', 'Value', 'Unit', 'Term Source Ref', 'Term Accession Number'] );
 
+my @a_dose_response = ( ['Sample Name', 'Assay Name', 'Protocol REF', 'Parameter Value [insecticidal substance]', 'Term Source Ref', 'Term Accession Number', 'Parameter Value [duration of exposure]', 'Unit', 'Term Source Ref', 'Term Accession Number', 'Characteristics [sample size (VBcv:0000983)]', 'Comment [note]', 'Raw Data File' ]);
 
+my @p_dose_response = ( ['Assay Name', 'Phenotype Name', 'Observable', 'Term Source Ref', 'Term Accession Number', 'Attribute', 'Term Source Ref', 'Term Accession Number', 'Comment [note]', 'Value', 'Unit', 'Term Source Ref', 'Term Accession Number' ]);
 
 #
 # This is the main loop for generating ISA-Tab data
@@ -85,6 +87,7 @@ my @p_IR_WHO = ( ['Assay Name', 'Phenotype Name', 'Observable', 'Term Source Ref
 # then use a four (!) level hash to remember which Assay Name to use for each combination of Village, Date and Location
 my %collection_name;
 my %who_dt_name;
+my %who_dr_name;
 #my $collection_counter = 1;
 
 # do a wildcard file "search" - results are a list of filenames that match
@@ -103,7 +106,6 @@ foreach my $filename (glob "$indir/*.{txt,tsv}") {
   foreach my $row (@$lines_aoh) {
     next unless ($row->{'Collection site'}); # skips empty lines
 
-  #  foreach my $genus (qw/ANOPHELES CULEX AEDES/) {
 
     my $sample_name = tidy_up_name(
 				   sprintf "%s.%s",
@@ -114,11 +116,11 @@ foreach my $filename (glob "$indir/*.{txt,tsv}") {
 
     # create collection assay name
     #To make my assay names for collection
-    my $assay_protocol_ref = assay_protocol_ref( $row->{"Protocol"} );
     my $collection_protocol_ref = 'COLL_OVI';
     my $collection_date = $row->{"Collection date"};
     my $a_collection_assay_name = $collection_name{$row->{"Collection site"}}{$row->{"Collection date"}} {$collection_protocol_ref} //= tidy_up_name(sprintf "%s.%s.%s", $row->{"Collection site"}, $row->{"Collection date"}, $collection_protocol_ref);
-    my $a_who_dt_assay_name = $who_dt_name{$sample_name} {$assay_protocol_ref} {$row->{"Insecticide"}} //= tidy_up_name(sprintf "%s.%s.%s", $sample_name, $assay_protocol_ref, $row->{"Insecticide"});
+    my $a_dt_assay_name = $who_dt_name{$sample_name} {$row->{"Insecticide"}} //= tidy_up_name(sprintf "%s.%s.%s", $sample_name, $row->{"Insecticide"}, 'dt');
+    my $a_dr_assay_name = $who_dr_name{$sample_name} {$row->{"Insecticide"}} //= tidy_up_name(sprintf "%s.%s.%s", $sample_name, $row->{"Insecticide"}, 'dr');
 
     push @a_species, [ $sample_name, "$sample_name.SPECIES", '', 'SPECIES', 'Aedes aegypti', 'VBsp', '0000518' ];
 
@@ -127,30 +129,56 @@ foreach my $filename (glob "$indir/*.{txt,tsv}") {
     push @s_samples, [ $row->{"Source of the data"}, $sample_name, '', 'pool', 'EFO', '0000663', 'female', 'PATO', '0000383', dev_stage($row->{"Mosquitoes tested"}) ];
 
 
-    my $num_mozzies_total = $row->{"No. Mosquitoes"};
-    if (length($num_mozzies_total) > 0 &&
-	!looks_like_number($num_mozzies_total)) {
-      die "didn't like num mozzies >$num_mozzies_total<\n";
+    my $num_mozzies = $row->{"No. Mosquitoes"};
+    if (length($num_mozzies) > 0 &&
+	!looks_like_number($num_mozzies)) {
+      die "didn't like num mozzies >$num_mozzies<\n";
     }
-    # so $num_mozzies_total could still be empty string
-
-    my $num_mozzies_WHO = $num_mozzies_total;
-    if ($row->{"Mosquitoes tested"} =~ /^.+larvae$/) {
-      die "need to half the num mozzie total >$num_mozzies_total<\n";
+    # decide which types of assay need outputting:
+    my $need_dt_rows = 0;
+    my $need_dr_rows = 0;
+    my $lc95 = $row->{"LC 95"};
+    my $lc99 = $row->{"LC 99"};
+    my $percent_mortality = $row->{"Percent mortality"};
+    my $lc50 = $row->{"LC 50"};
+    if (defined $percent_mortality && length($percent_mortality) > 0 &&
+	defined $lc50 && length($lc50) > 0 && looks_like_number($lc50)) {
+      $need_dt_rows = 1;
+      $need_dr_rows = 1;
+      # when both are printed the sample size is shared between them:
+      $num_mozzies = $num_mozzies/2;
+    } elsif (defined $lc50 && length($lc50) > 0 && looks_like_number($lc50)) {
+      $need_dr_rows = 1;
+    } elsif (defined $percent_mortality && length($percent_mortality) > 0) {
+      $need_dt_rows = 1;
+    } else {
+      die "fatal error: assay confusion >$sample_name<\n";
     }
-    # TO DO - decide if all or some mozzies are used in the WHO assay
 
-push @a_IR_WHO, [ $sample_name, $a_who_dt_assay_name, $assay_protocol_ref, insecticide_term($row->{"Insecticide"}), $row->{"Concentration"}, concentration_unit_term( $row->{"Units"}), duration_and_units($row->{"Duration"}), $num_mozzies_WHO, 'p_IR_WHO.txt' ];
+    my @concentration_unit_term = concentration_unit_term($row->{"Units"});
+    my @insecticide_term = insecticide_term($row->{"Insecticide"});
+    # now output them
+    if ($need_dt_rows) {
+      my $assay_protocol_ref = dt_protocol_ref($row->{'Mosquitoes tested'}, $row->{'Protocol'});
+      push @a_IR_WHO, [ $sample_name, $a_dt_assay_name, $assay_protocol_ref, @insecticide_term, $row->{"Concentration"}, @concentration_unit_term, duration_and_units($row->{"Duration"}), $num_mozzies, 'IA', 'p_IR_WHO.txt' ];
 
-    push @p_IR_WHO, [ $a_who_dt_assay_name, "Mortality percentage:$row->{'Percent mortality'},$row->{'Concentration'}% insecticide_term($row->{'Insecticide'})", 'insecticide resistance', 'MIRO', '00000021', 'mortality rate', 'VBcv', '0000703', 'IA', $row->{"Percent mortality"}, 'percent', 'UO', '0000187'];
+      push @p_IR_WHO, [ $a_dt_assay_name, "Mortality percentage: $row->{'Percent mortality'}%, $row->{'Concentration'} $concentration_unit_term[0] $insecticide_term[0]", 'insecticide resistance', 'MIRO', '00000021', 'mortality rate', 'VBcv', '0000703', 'IA', $row->{"Percent mortality"}, 'percent', 'UO', '0000187'];
+    }
+
+    if ($need_dr_rows) {
+      my $assay_protocol_ref = dr_protocol_ref($row->{'Mosquitoes tested'}, $row->{'Protocol'});
+
+      push @a_dose_response, [ $sample_name, $a_dr_assay_name, $assay_protocol_ref, @insecticide_term, duration_and_units($row->{"Duration"}), $num_mozzies, 'IA', 'p_dose-response.txt'];
+
+      push @p_dose_response, [ $a_dr_assay_name, "LC50: $lc50 $concentration_unit_term[0] $insecticide_term[0]", 'resistance to single insecticide', 'MIRO', '00000021', 'LC50', 'VBcv', '0000726', 'IC', $row->{'LC 50'}, @concentration_unit_term ];
+
+      push @p_dose_response, [ $a_dr_assay_name, "LC95: $lc95 $concentration_unit_term[0] $insecticide_term[0]", 'resistance to single insecticide', 'MIRO', '00000021', 'LC95', 'VBcv', '0000728', 'IC', $row->{'LC 95'}, @concentration_unit_term ];
+
+      push @p_dose_response, [ $a_dr_assay_name, "LC99: $lc99 $concentration_unit_term[0] $insecticide_term[0]", 'resistance to single insecticide', 'MIRO', '00000021', 'LC99', 'VBcv', '0000729', 'IC', $row->{'LC 99'}, @concentration_unit_term ];
+
+    }
   }
- }
-
-
-# printing an array with tab separators
-# print join("\t", @headings)."\n";
-
-
+}
 
 
 write_table("$outdir/s_samples.txt", \@s_samples);
@@ -158,7 +186,8 @@ write_table("$outdir/a_species.txt", \@a_species);
 write_table("$outdir/a_collection.txt", \@a_collection);
 write_table("$outdir/a_IR_WHO.txt", \@a_IR_WHO);
 write_table("$outdir/p_IR_WHO.txt", \@p_IR_WHO);
-
+write_table("$outdir/a_dose_response.txt", \@a_dose_response);
+write_table("$outdir/p_dose_response.txt", \@p_dose_response);
 #
 # LOOKUP SUBS
 #
@@ -195,23 +224,48 @@ sub dev_stage {
   }
 }
 
-sub assay_protocol_ref {
-  my $input = shift;
-  given ($input) {
-    when (/^WHO ?1981 ?bioassay$/i) {
-      return ('IR_WHO_1981')
+sub dt_protocol_ref {
+  my ($dev_stage, $protocol) = @_;
+  if ($dev_stage =~ /larvae/i) {
+    if ($protocol =~ /WHO/i) {
+      return ("WHO_larvicide_DT");
+    }else {
+      die "unexpected dev stage and protocol combination in dt_protocol_ref >$dev_stage< >$protocol<\n";
     }
-    when (/^WHO ?2016 ?bioassay$/i) {
-      return ('IR_WHO_2016')
+  }elsif ($dev_stage =~ /adult/i  || $dev_stage eq '') { # one rockefellar control missing dev stage should have been adult
+    if ($protocol =~ /WHO/i) {
+      return ("WHO_paper_DT");
+    }elsif ($protocol =~ /bottle/i) {
+      return ("bottle_DT");
+    }else {
+      die "unexpected dev_stage and protocol combination in dt_protocol_ref >$dev_stage> >$protocol<\n";
     }
-    when (/^CDC ?bottle ?assay$/i) {
-      return ('IR_CDC_BOTTLE')
-    }
-    default {
-      die "fatal error: unknown who_protocol_ref >$input<\n";
-    }
+  }else {
+    die "unexpected dev stage and protocol combination in dt_protocol_ref >$dev_stage< >$protocol<\n";
   }
 }
+
+sub dr_protocol_ref {
+  my ($dev_stage, $protocol) = @_;
+  if ($dev_stage =~ /larvae/i) {
+    if ($protocol =~ /WHO/i) {
+      return ("WHO_larvicide_DR");
+    }else {
+      die "unexpected dev stage and protocol combination in dr_protocol_ref >$dev_stage< >$protocol<\n";
+    }
+  }elsif ($dev_stage =~ /adult/i) {
+    if ($protocol =~ /WHO/i) {
+      return ("WHO_paper_DR");
+    }elsif ($protocol =~ /bottle/i) {
+      return ("bottle_DR");
+    }else {
+      die "unexpected dev_stage and protocol combination in dr_protocol_ref >$dev_stage< >$protocol<\n";
+    }
+  }else {
+    die "unexpected dev stage and protocol combination in dr_protocol_ref >$dev_stage< >$protocol<\n";
+  }
+}
+
 sub num_mozzies {
   my $input = shift;
   if (looks_like_number($input)) {
